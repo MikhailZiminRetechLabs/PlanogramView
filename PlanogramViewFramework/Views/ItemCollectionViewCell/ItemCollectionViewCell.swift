a8b015682acb34dad91c6d479fb4d4412931220c
Mikhail Zimin <mikhail@retechlabs.com>
2019-09-16 16:26:23 +0700

    Open ItemCollectionViewCell.

--- PlanogramViewFramework/Views/ItemCollectionViewCell/ItemCollectionViewCell.swift
+++ PlanogramViewFramework/Views/ItemCollectionViewCell/ItemCollectionViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class ItemCollectionViewCell: UICollectionViewCell {
+open class ItemCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet public weak var stackView: UIStackView!
     
